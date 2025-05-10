740083dd8a29fa7d37a6e0a81b006f49b81cd0e8
Gulliver2405 <gulliver.de.clercq@icloud.com>
2025-05-10 16:34:47 +0200

    RisingWater + Camera
    Verbetering van de camera movement en de toevoeging van 'risingWater' script

--- Assets/scripts/MoveCamera.cs
+++ Assets/scripts/MoveCamera.cs
@@ -2,27 +2,21 @@ using UnityEngine;
 
 public class MoveCamera : MonoBehaviour
 {
-    [SerializeField] private float scrollSpeed = 1.25f;
-    [SerializeField] private float delayBeforeStart = 5f;
+    [SerializeField] private Transform player1;
+    [SerializeField] private Transform player2;
+    [SerializeField] private float smoothTime = 0.25f;
+    [SerializeField] private float fixedX = 0f;
+    [SerializeField] private float fixedZ = -10f;
 
-    [SerializeField]private float timer = 0f;
-    private bool startMoving = false;
+    private Vector3 velocity = Vector3.zero;
 
-    void Update()
+    void LateUpdate()
     {
-        // Wait for delay time
-        if (!startMoving)
-        {
-            timer += Time.deltaTime;
-            if (timer >= delayBeforeStart)
-            {
-                startMoving = true;
-            }
-        }
-        else
-        {
-            // Move the camera up
-            transform.position += Vector3.up * scrollSpeed * Time.deltaTime;
-        }
+        float highestY = Mathf.Max(player1.position.y, player2.position.y);
+
+        float targetY = Mathf.Max(highestY, transform.position.y);
+
+        Vector3 targetPos = new Vector3(fixedX, targetY, fixedZ);
+        transform.position = Vector3.SmoothDamp(transform.position, targetPos, ref velocity, smoothTime);
     }
 }
---
+++ Assets/scripts/RisingWater.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+
+public class RisingWater : MonoBehaviour
+{
+    [SerializeField] private float scrollSpeed = 1.25f;
+    [SerializeField] private float delayBeforeStart = 5f;
+    [SerializeField] private GameObject player1;
+    [SerializeField] private GameObject player2;
+
+    private GameObject PlayerWhoDied;
+    private float timer = 0f;
+    private bool startMoving = false;
+
+    private Collider2D waterCollider;
+
+    void Start()
+    {
+        waterCollider = GetComponent<Collider2D>();
+        if (!waterCollider.isTrigger)
+            waterCollider.isTrigger = true;
+    }
+
+    void Update()
+    {
+        if (!startMoving)
+        {
+            timer += Time.deltaTime;
+            if (timer >= delayBeforeStart)
+                startMoving = true;
+        }
+        else
+        {
+            transform.position += Vector3.up * scrollSpeed * Time.deltaTime;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject == player1)
+        {
+            PlayerWhoDied = player1;
+            KillPlayers();
+        }
+        if(other.gameObject == player2)
+        {
+            PlayerWhoDied = player2;
+            KillPlayers();
+        }
+    }
+
+    void KillPlayers()
+    {
+        Debug.Log($"Game Over! {PlayerWhoDied.name} lost!");
+        Time.timeScale = 0f;
+    }
+}
