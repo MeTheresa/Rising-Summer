2737131b67b8665bd7400a0320ac27fdefebcb99
KeanoDAE <keanobaeke1@gmail.com>
2025-05-25 16:52:25 +0200

    Main menu navigation
    added main menu navigation with controller and keyboard

--- Assets/scripts/MainMenuScript.cs
+++ Assets/scripts/MainMenuScript.cs
@@ -1,23 +1,108 @@
 using UnityEngine;
-using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+using UnityEngine.Windows;
 using UnityEngine.UI;
+using System.Collections;
 
 public class MainMenuScript : MonoBehaviour
 {
-    [SerializeField] private GameObject _menuMain;
-    void Awake()
+    [SerializeField] private GameObject menuMain;
+    [SerializeField] private GameObject menuPlay;
+    [SerializeField] private GameObject menuControls;
+
+    [SerializeField] private GameObject playButton;
+    [SerializeField] private GameObject controlsButton;
+    [SerializeField] private GameObject startButton;
+    [SerializeField] private GameObject backButtonFromPlay;
+    [SerializeField] private GameObject backButtonFromControls;
+
+    private MenuControls input;
+    private void Awake()
     {
-        if (_menuMain.activeSelf == false)
+        input = new MenuControls();
+
+        input.Menu.Submit.performed += ctx => OnSubmit();
+        input.Menu.GoBack.performed += ctx => OnGoBack();
+
+        ShowMainMenu();
+    }
+
+    private void OnEnable() => input.Enable();
+    private void OnDisable() => input.Disable();
+
+    private void OnSubmit()
+    {
+        var selectedGO = EventSystem.current.currentSelectedGameObject;
+        if (selectedGO == null) return;
+
+        if (selectedGO == playButton)
         {
-            Debug.Log("Not active");
-            _menuMain.SetActive(true);
-            Debug.Log("Active");
+            ShowPlayMenu();
+            return;
+        }
+        else if (selectedGO == controlsButton)
+        {
+            ShowControlsMenu();
+            return;
+        }
+        else if (selectedGO == backButtonFromPlay || selectedGO == backButtonFromControls)
+        {
+            ShowMainMenu();
+            return;
+        }
+
+        var button = selectedGO.GetComponent<Button>();
+        if (button != null)
+        {
+            button.onClick.Invoke();
         }
     }
-    
+
+    private void OnGoBack()
+    {
+        if (menuPlay.activeSelf || menuControls.activeSelf)
+        {
+            ShowMainMenu();
+        }
+    }
+
+    private void ShowMainMenu()
+    {
+        menuMain.SetActive(true);
+        menuPlay.SetActive(false);
+        menuControls.SetActive(false);
+
+        StartCoroutine(SetSelectedNextFrame(playButton));
+    }
+
+    private void ShowPlayMenu()
+    {
+        menuMain.SetActive(false);
+        menuPlay.SetActive(true);
+        menuControls.SetActive(false);
+
+        StartCoroutine(SetSelectedNextFrame(startButton));
+    }
+
+    private void ShowControlsMenu()
+    {
+        menuMain.SetActive(false);
+        menuPlay.SetActive(false);
+        menuControls.SetActive(true);
+
+        StartCoroutine(SetSelectedNextFrame(backButtonFromControls));
+    }
+
     public void ChangeSceneToPlayScene()
     {
         SceneManager.LoadScene("SceneLevel_1");
+    }
+
+    private IEnumerator SetSelectedNextFrame(GameObject button)
+    {
+        yield return null;
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(button);
     }
 }
